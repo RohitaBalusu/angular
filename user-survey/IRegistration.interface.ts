5b65fc3775734aa2f623b4b69518cf3b9dc8537f
RohitaBalusu <68987348+RohitaBalusu@users.noreply.github.com>
2021-03-18 21:08:52 +0530

    Add files via upload

---
+++ user-survey/IRegistration.interface.ts
@@ -0,0 +1,7 @@
+export interface IRegistration {
+    firstName: string;
+    lastName: string;
+    emailId: string;
+    mobile: string;
+    password: string;
+}
---
+++ user-survey/ISurvey.interface.ts
@@ -0,0 +1,10 @@
+export interface ISurvey {
+    id: number;
+    title: string;
+    description: string;
+    city: string;
+    state: string;
+    country: string;
+    zip: string;
+    surveyDate: string;
+}
