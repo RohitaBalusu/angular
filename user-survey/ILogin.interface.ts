c2d7d7fc928aeb303a0508471586291db67d3a6e
RohitaBalusu <68987348+RohitaBalusu@users.noreply.github.com>
2021-03-18 21:09:28 +0530

    Delete user-survey directory

--- user-survey/ILogin.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface ILogin {
-   
-    emailId: string;
-    
-    password: string;
-}
--- user-survey/IRegistration.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface IRegistration {
-    firstName: string;
-    lastName: string;
-    emailId: string;
-    mobile: string;
-    password: string;
-}
